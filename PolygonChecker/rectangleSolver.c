3794973d9d1b022e6cb272d029b50886c7c2fe24
Oluwaloni <ayenimidun@gmail.com>
2025-11-11 16:40:19 -0500

    Improved rectangleSolver.c by adding explicit duplicate coordinate validation

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -8,6 +8,13 @@ static int dist2(int x1, int y1, int x2, int y2)
 
 int isRectangle(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
 {
+	if ((x1 == x2 && y1 == y2) ||
+		(x2 == x3 && y2 == y3) ||
+		(x3 == x4 && y3 == y4) ||
+		(x4 == x1 && y4 == y1))
+
+		return 0;
+
 	int d1 = dist2(x1, y1, x2, y2);
 	int d2 = dist2(x2, y2, x3, y3);
 	int d3 = dist2(x3, y3, x4, y4);
