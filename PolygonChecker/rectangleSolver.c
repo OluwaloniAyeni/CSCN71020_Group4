7b15059728803787553882dedc183f03c67706dd
AFSANA ZAMAN <Azaman6489@conestogac.on.ca>
2025-11-16 23:13:27 -0500

    Added function prototypes (calculateArea, calculatePerimeter) in rectangleSolver.h.

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -32,6 +32,7 @@ int isRectangle(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
 	int diag1 = dist2(x1, y1, x3, y3);
 	int diag2 = dist2(x2, y2, x4, y4);
 
+
 if (d1 > 0 && d2 > 0 && d1 == d3 && d2 == d4 && diag1 == diag2 && dot(x1,y1,x2,y2,x3,y3,x4,y4)==0)
       return 1;
 
